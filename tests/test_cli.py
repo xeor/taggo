c6c4e18632685e0f0fd36cc8c25a96fc9f21b8cd
Lars Solberg <lars.solberg@gmail.com>
2017-10-12 23:04:51 +0200

    Fixing up tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import io
-import contextlib
-
 import pytest
 import taggo
 
 
-def runner(args, required_text, stderr=False):
-    f = io.StringIO()
-    redirector = contextlib.redirect_stderr if stderr else contextlib.redirect_stdout
-    with redirector(f):
-        try:
-            taggo.main(args)
-        except SystemExit:
-            pass
-    assert required_text in f.getvalue()
+def test_noargs(capsys):
+    with pytest.raises(SystemExit) as ex:
+        taggo.main([])
+    assert ex.value.code == 2
+
+    out, err = capsys.readouterr()
+    assert "the following arguments are required" in err
 
 
-def test_noargs():
-    runner([], 'the following arguments are required', stderr=True)
+def test_help(capsys):
+    with pytest.raises(SystemExit) as ex:
+        taggo.main(["-h"])
+    assert ex.value.code == 0
+
+    out, err = capsys.readouterr()
+    assert "show this help message and exit" in out
 
 
-def test_help():
-    runner(['-h'], 'show this help message and exit')
+def test_run_non_existing(capsys):
+    taggo.main(["--debug", "run", "non-existing", "temp/test_run_non_existing"])
+    out, err = capsys.readouterr()
+    assert "ERROR - Didnt find src-path" in err
 
 
-def test_run_non_existing():
-    runner(['run', 'a', 'b'], '')
+def test_run_dry(capsys):
+    taggo.main(["--debug", "run", "--dry", "tests/tagged_files/", "temp/test_run_dry"])
+    out, err = capsys.readouterr()
+    assert "making tag-directory" in err
+    assert "making symlink" in err
+    assert "Checking directory" in err
+    assert "to: ../../../tests/tagged_files/folders/pictures/2012 #Oslo tour/fishes/file #tag.jpg" in err
