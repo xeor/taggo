9174d0a9bc83a74dfd8886d5d6271978b17d478f
Lars Solberg <lars.solberg@gmail.com>
2017-10-14 23:35:09 +0200

    Rename and cleanup

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -33,24 +33,24 @@ def test_run_non_existing(capsys):
 
 
 def test_run_dry(capsys):
-    taggo.main(["--debug", "run", "--dry", "tests/tagged_stuff/", "temp/test_run_dry"])
+    taggo.main(["--debug", "run", "--dry", "tests/test_files/tagged/", "temp/test_run_dry"])
     out, err = capsys.readouterr()
     assert "making tag-directory" in err
     assert "making symlink" in err
     assert "Checking directory" in err
-    assert "to: ../../../tests/tagged_stuff/folders/pictures/2012 #Oslo tour/fishes/file #tag.jpg" in err
+    assert "to: ../../../tests/test_files/tagged/folders/pictures/2012 #Oslo tour/fishes/file #tag.jpg" in err
 
 
 def test_nonexisting_dst(capsys):
     tmp = "temp/test_nonexisting_dst/{}".format(str(random.random()))
-    taggo.main(["run", "tests/tagged_stuff/", tmp])
+    taggo.main(["run", "tests/test_files/tagged/", tmp])
     assert os.path.isdir('{}/root'.format(tmp))
 
 
 def test_existing_dst(capsys):
     if not os.path.isdir("temp/test_existing_dst"):
         os.mkdir("temp/test_existing_dst")
-    taggo.main(["--debug", "run", "tests/tagged_stuff/", "temp/test_existing_dst"])
+    taggo.main(["--debug", "run", "tests/test_files/tagged/", "temp/test_existing_dst"])
     out, err = capsys.readouterr()
     assert "dst path exists and is a folder" in err
 
@@ -59,11 +59,11 @@ def test_existing_file_dst(capsys):
     with open("temp/test_existing_file_dst", "w") as fp:
         fp.write("")
     with pytest.raises(taggo.FolderException, message="dst exist but is not a folder. Cant continue"):
-        taggo.main(["--debug", "run", "tests/tagged_stuff/", "temp/test_existing_file_dst"], reraise=True)
+        taggo.main(["--debug", "run", "tests/test_files/tagged/", "temp/test_existing_file_dst"], reraise=True)
 
 
 def test_symlink_creation(capsys):
-    taggo.main(["run", "tests/tagged_stuff/", "temp/test_symlink_creation"])
+    taggo.main(["run", "tests/test_files/tagged/", "temp/test_symlink_creation"])
 
     for f in [
         "tagfolder/a #tagfolder - a #tagfolder/folder #with-a-deep-tag #multitag folder/a_file",
@@ -71,3 +71,14 @@ def test_symlink_creation(capsys):
         "with/a/deep/tag/a #tagfolder_folder #with-a-deep-tag #multitag folder - folder #with-a-deep-tag #multitag folder/a_file"
     ]:
         assert os.path.isfile("temp/test_symlink_creation/{}".format(f))
+
+
+def test_via_python_command(capsys):
+    import subprocess
+    proc = subprocess.Popen(["python3", "./taggo/__init__.py", "-h"], stdout=subprocess.PIPE)
+    assert proc.wait() == 0
+    assert b"show this help message and exit" in proc.stdout.read()
+
+
+def test_cleanup(capsys):
+    taggo.main(["cleanup", "temp/cleanup_test"])
