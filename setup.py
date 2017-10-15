54dfed14ebd1a06c888bc3c48731af12add62f2f
Lars Solberg <lars.solberg@gmail.com>
2017-10-15 23:05:16 +0200

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='taggo',
-    version='1.0.0',
+    version='1.1.0',
     description="Tag organizer that creates symlinks from filename-tags",
     long_description=readme + '\n\n' + history,
     author="Lars Solberg",
