d3e8f58490da975e17499cb28f1d0b1f67c6cb10
Lars Solberg <lars.solberg@gmail.com>
2017-11-04 21:44:07 +0100

    Bump version: 0.9.1 → 0.10.0

--- taggo/__init__.py
+++ taggo/__init__.py
@@ -8,7 +8,7 @@ from . import (exceptions, utils)
 
 __author__ = """Lars Solberg"""
 __email__ = 'lars.solberg@gmail.com'
-__version__ = '0.9.1'
+__version__ = '0.10.0'
 
 logger = logging.getLogger("taggo")
 hashtag_re = re.compile(r'\B#([^ \.,]+)\b')
